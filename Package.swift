d03caabb7813a790c9acf6a87e1869db0ae97209
Cemal <ctuysuz18@posta.pau.edu.tr>
2025-05-03 18:29:07 +0300

    Dependencies updated 📦

--- Package.swift
+++ Package.swift
@@ -7,24 +7,28 @@ let package = Package(
        .macOS(.v13)
     ],
     dependencies: [
-        // 💧 A server-side Swift web framework.
         .package(url: "https://github.com/vapor/vapor.git", from: "4.110.1"),
-        // 🗄 An ORM for SQL and NoSQL databases.
-        .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
-        // 🐘 Fluent driver for Postgres.
-        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.8.0"),
-        // 🔵 Non-blocking, event-driven networking for Swift. Used for custom executors
         .package(url: "https://github.com/apple/swift-nio.git", from: "2.65.0"),
+        .package(url: "https://github.com/vapor/queues-redis-driver.git", from: "1.1.1"),
+        .package(url: "https://github.com/vapor/fluent.git", from: "4.9.0"),
+        .package(url: "https://github.com/vapor/fluent-postgres-driver.git", from: "2.9.2"),
+        .package(url: "https://github.com/vapor/jwt.git", from: "4.2.2"),
+        .package(url: "https://github.com/vapor-community/Lingo-Vapor.git", from: "4.2.0"),
+        .package(url: "https://github.com/Joannis/VaporSMTPKit.git", from: "1.0.0")
     ],
     targets: [
         .executableTarget(
             name: "Vapor-Quick-Start-Template",
             dependencies: [
-                .product(name: "Fluent", package: "fluent"),
-                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
                 .product(name: "Vapor", package: "vapor"),
                 .product(name: "NIOCore", package: "swift-nio"),
                 .product(name: "NIOPosix", package: "swift-nio"),
+                .product(name: "Fluent", package: "fluent"),
+                .product(name: "FluentPostgresDriver", package: "fluent-postgres-driver"),
+                .product(name: "JWT", package: "jwt"),
+                .product(name: "QueuesRedisDriver", package: "queues-redis-driver"),
+                .product(name: "LingoVapor", package: "Lingo-Vapor"),
+                .product(name: "VaporSMTPKit", package: "VaporSMTPKit")
             ],
             swiftSettings: swiftSettings
         ),
